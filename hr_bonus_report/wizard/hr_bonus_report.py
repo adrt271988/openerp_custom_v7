511a3642ca99b1efd454b40beaaa59acf77ecd28
adrt271988 <adrt271988@gmail.com>
2016-05-07 08:52:15 -0430

    Modificando consulta para calcular acumulados de decimos para hr_bonus_report

--- hr_bonus_report/wizard/hr_bonus_report.py
+++ hr_bonus_report/wizard/hr_bonus_report.py
@@ -13,13 +13,18 @@ class HrBonusReporWizard(osv.osv_memory):
         wizard = self.browse(cr, uid, ids)[0]
         date_from = wizard.from_period.date_start
         date_to = wizard.to_period.date_stop
+        report_type = wizard.report_type
         employee_ids = [employee.id for employee in wizard.employee_ids]
-        string = employee_ids and "employee_id in %s AND"%tuple(employee_ids) or ""
-        cr.execute(""" SELECT pay.period_id,pay.employee_id,em.name_related FROM hr_payslip AS pay
-                        LEFT JOIN hr_employee AS em ON em.id = pay.employee_id
-                        WHERE pay.employee_id in (921,794) AND pay.date_to BETWEEN '2016-01-01' AND '2016-02-29'
-                        GROUP BY pay.employee_id,em.name_related,pay.period_id
-                        ORDER BY em.name_related"""%(string,date_from,date_to))
+        clause1 = employee_ids and "employee_id in %s AND"%tuple(employee_ids) or ""
+        clause2 = report_type == "all" and "pline.code in ('PROV DTERCERO','PROV DCUARTO')" or "pline.code = '%s'"%report_type
+        cr.execute(""" SELECT pay.employee_id,em.name_related,em.gender,em.identification_id,
+                            job.name AS job_name,SUM(pline.amount) as acum FROM hr_payslip AS pay
+                        JOIN hr_employee AS em ON em.id = pay.employee_id
+                        LEFT JOIN hr_job AS job ON job.id = em.job_id
+                        JOIN hr_payslip_line AS pline ON pline.slip_id = pay.id
+                        WHERE %s pay.date_to BETWEEN '%s' AND '%s'AND %s
+                        GROUP BY pay.employee_id,em.name_related,em.gender,em.identification_id,job_name
+                        ORDER BY em.name_related"""%(clause1,date_from,date_to,clause2))
         res = cr.dictfetchall()
         print '******',res
         if employee_ids:
@@ -59,7 +64,7 @@ class HrBonusReporWizard(osv.osv_memory):
         'period_id': fields.many2one("account.period", string="Periodo"),
         'from_period': fields.many2one("account.period", string="Periodo Inicio"),
         'to_period': fields.many2one("account.period", string="Periodo Fin"),
-        'report_type': fields.selection([('third', "Tercero"),('fourth', "Cuarto"),('all',"Todo")], string="Tipo de Reporte", required=True),
+        'report_type': fields.selection([('PROV DTERCERO', "Tercero"),('PROV DCUARTO', "Cuarto"),('all',"Todo")], string="Tipo de Reporte", required=True),
         'employee_ids': fields.many2many('hr.employee', 'hr_bonus_report_wizard_employee_rel', 'wizard_id', 'employee_id', string="Empleados"),
         'date_from': fields.date("Desde"),
         'date_to': fields.date("Hasta"),
