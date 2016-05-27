66f9180daa3f8132cff763580269e1289107da6a
rodrigueza <rodrigueza@erksoft.com.ve>
2016-05-27 13:53:07 -0430

    Modificaciones de nombres de archivos en custom_accounting_report_webkit_xls... Aun en desarrollo

--- custom_accounting_report_webkit_xls/wizard/account_financial_report.py
+++ custom_accounting_report_webkit_xls/wizard/account_financial_report.py
@@ -22,33 +22,94 @@ from osv import fields, osv
 #import logging
 #_logger = logging.getLogger(__name__)
 
-class account_financial_report(osv.osv_memory):
-    _inherit = 'accounting.report'
+class account_financial_report_xls(osv.osv_memory):
+
+    #~ _inherit = "account.common.balance.report"
+    _inherit = "accounting.report"
+    _name = "accounting.report.xls"
+    _description = "Balance Sheet Report"
        
     def xls_export(self, cr, uid, ids, context=None):
-        res= self.check_report(cr, uid, ids, context=context)
+        res = self.check_report(cr, uid, ids, context=context)
         return res
 
     def _print_report(self, cr, uid, ids, data, context=None):
         context = context or {}
+        data['form'].update(self.read(cr, uid, ids, ['date_from_cmp',  'debit_credit', 'date_to_cmp',  'fiscalyear_id_cmp', 'period_from_cmp', 'period_to_cmp',  'filter_cmp', 'account_report_id', 'enable_filter', 'label_filter','target_move'], context=context)[0])
         if context.get('xls_export'):
             # we update form with display account value
             data = self.pre_print_report(cr, uid, ids, data, context=context)
             cuenta=self.browse(cr, uid,ids, context=None)
             data['form']['type'] = cuenta[0].type
             data['form']['niveles']=cuenta[0].niveles
-
-
             return {'type': 'ir.actions.report.xml',
                     'report_name': 'account.account_report_financial_xls',
                     'datas': data}
         else:
-            return super(account_financial_report, self)._print_report(cr, uid, ids, data, context=context)
+           return {
+                'type': 'ir.actions.report.xml',
+                'report_name': 'account.financial.report',
+                'datas': data,
+            }
 
-    # def check_report(self, cr, uid, ids, context=None):
-    #     res = {}
-    #     # super('accounting.report', self).check_report(cr, uid, ids, context)
-    #     # nueva_var=res['datas']['form']['comparison_context']
-    #     return super('accounting.report', self).check_report(cr, uid, ids, context)
+    _columns = {
+        'enable_filter': fields.boolean('Enable Comparison'),
+        'account_report_id': fields.many2one('account.financial.report', 'Account Reports', required=True),
+        'label_filter': fields.char('Column Label', size=32, help="This label will be displayed on report to show the balance computed for the given comparison filter."),
+        'fiscalyear_id_cmp': fields.many2one('account.fiscalyear', 'Fiscal Year', help='Keep empty for all open fiscal year'),
+        'filter_cmp': fields.selection([('filter_no', 'No Filters'), ('filter_date', 'Date'), ('filter_period', 'Periods')], "Filter by", required=True),
+        'period_from_cmp': fields.many2one('account.period', 'Start Period'),
+        'period_to_cmp': fields.many2one('account.period', 'End Period'),
+        'date_from_cmp': fields.date("Start Date"),
+        'date_to_cmp': fields.date("End Date"),
+        'debit_credit': fields.boolean('Display Debit/Credit Columns', help="This option allows you to get more details about the way your balances are computed. Because it is space consuming, we do not allow to use it while doing a comparison."),
+    }
+
+    def _get_account_report(self, cr, uid, context=None):
+        # TODO deprecate this it doesnt work in web
+        menu_obj = self.pool.get('ir.ui.menu')
+        report_obj = self.pool.get('account.financial.report')
+        report_ids = []
+        if context.get('active_id'):
+            menu = menu_obj.browse(cr, uid, context.get('active_id')).name
+            report_ids = report_obj.search(cr, uid, [('name','ilike',menu)])
+        return report_ids and report_ids[0] or False
+
+    _defaults = {
+            'filter_cmp': 'filter_no',
+            'target_move': 'posted',
+            'account_report_id': _get_account_report,
+    }
     
-account_financial_report()
+    def _build_comparison_context(self, cr, uid, ids, data, context=None):
+        if context is None:
+            context = {}
+        result = {}
+        result['fiscalyear'] = 'fiscalyear_id_cmp' in data['form'] and data['form']['fiscalyear_id_cmp'] or False
+        result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
+        result['chart_account_id'] = 'chart_account_id' in data['form'] and data['form']['chart_account_id'] or False
+        result['state'] = 'target_move' in data['form'] and data['form']['target_move'] or ''
+        if data['form']['filter_cmp'] == 'filter_date':
+            result['date_from'] = data['form']['date_from_cmp']
+            result['date_to'] = data['form']['date_to_cmp']
+        elif data['form']['filter_cmp'] == 'filter_period':
+            if not data['form']['period_from_cmp'] or not data['form']['period_to_cmp']:
+                raise osv.except_osv(_('Error!'),_('Select a starting and an ending period'))
+            result['period_from'] = data['form']['period_from_cmp']
+            result['period_to'] = data['form']['period_to_cmp']
+        return result
+
+    def check_report(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        res = super(accounting_report, self).check_report(cr, uid, ids, context=context)
+        data = {}
+        data['form'] = self.read(cr, uid, ids, ['account_report_id', 'date_from_cmp',  'date_to_cmp',  'fiscalyear_id_cmp', 'journal_ids', 'period_from_cmp', 'period_to_cmp',  'filter_cmp',  'chart_account_id', 'target_move'], context=context)[0]
+        for field in ['fiscalyear_id_cmp', 'chart_account_id', 'period_from_cmp', 'period_to_cmp', 'account_report_id']:
+            if isinstance(data['form'][field], tuple):
+                data['form'][field] = data['form'][field][0]
+        comparison_context = self._build_comparison_context(cr, uid, ids, data, context=context)
+        res['datas']['form']['comparison_context'] = comparison_context
+        return res
+
+account_financial_report_xls()
